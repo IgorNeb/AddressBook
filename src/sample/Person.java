46407e615fb8789b84e1f03debecc45ca09c6450
Igor Pershko <globaltelnet7.62@gmail.com>
2016-11-26 14:41:40 +0200

    work

--- src/sample/Person.java
+++ src/sample/Person.java
@@ -1,31 +1,38 @@
 package sample;
 
+import javafx.beans.property.SimpleStringProperty;
+
 /**
  * Created by Nebo on 21.11.2016.
  */
 public class Person {
 
-    private String name;
-    private String tel;
+    private SimpleStringProperty name = new SimpleStringProperty("");
+    private SimpleStringProperty tel = new SimpleStringProperty("");
+
+    public Person() {}
 
     public Person(String name, String tel) {
-        this.name = name;
-        this.tel = tel;
+        this.name = new SimpleStringProperty(name);
+        this.tel = new SimpleStringProperty(tel);
     }
 
     public String getName() {
-        return name;
+        return name.get();
     }
 
     public void setName(String name) {
-        this.name = name;
+        this.name.set(name);
     }
 
     public String getTel() {
-        return tel;
+        return tel.get();
     }
 
     public void setTel(String tel) {
-        this.tel = tel;
+        this.tel.set(tel);
     }
+
+    public SimpleStringProperty propertyName() {return name;}
+    public SimpleStringProperty propertyTel() {return tel;}
 }
